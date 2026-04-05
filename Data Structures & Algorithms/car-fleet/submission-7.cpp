c49506b852f9c7d0b42dfac6a51a0ce5e424e593
Shaurya Dhingra <137608962+shauryad01@users.noreply.github.com>
2026-04-05 16:29:57 +0530

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> cars;
+
+        for(int i = 0; i < position.size(); i++){
+            cars.push_back({position[i], speed[i]});
+        }
+
+        sort(cars.begin(), cars.end(), greater<pair<int,int>>());
+
+        stack<double> fleets;
+
+        for(int i = 0; i < cars.size(); i++){
+            double time = (double)(target - cars[i].first) / cars[i].second;
+
+            if(fleets.empty() || time > fleets.top()){
+                fleets.push(time);
+            }
+        }
+
+        return fleets.size();
+    }
+};
